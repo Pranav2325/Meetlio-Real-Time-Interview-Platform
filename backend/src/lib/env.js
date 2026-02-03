fa183ea0d722f338ae31d52bd8883ed924cf894b
Pranav2325 <pranavdhondkar@gmail.com>
2026-02-03 19:54:37 +0530

    user sync with db from clerk

--- backend/src/lib/env.js
+++ backend/src/lib/env.js
@@ -1,8 +1,13 @@
-import dotenv from "dotenv"
-dotenv.config({quiet:true})
+import dotenv from "dotenv";
+dotenv.config({ quiet: true });
 
-export const ENV={
-    PORT:process.env.PORT,
-    DB_URL:process.env.DB_URL,
-    NODE_ENV:process.env.NODE_ENV
-}
+export const ENV = {
+  PORT: process.env.PORT,
+  DB_URL: process.env.DB_URL,
+  NODE_ENV: process.env.NODE_ENV,
+  CLIENT_URL: process.env.NODE_ENV,
+  INNGEST_EVENT_KEY: process.env.INNGEST_EVENT_KEY,
+  INNGEST_SIGNING_KEY: process.env.INNGEST_SIGNING_KEY,
+  STREAM_API_KEY: process.env.STREAM_API_KEY,
+  STREAM_SECRET_KEY: process.env.STREAM_SECRET_KEY,
+};
