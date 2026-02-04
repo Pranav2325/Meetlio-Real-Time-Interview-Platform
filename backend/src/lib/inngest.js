0fc0386f7674b20675faa49d26d67cb8c858f39c
Pranav2325 <pranavdhondkar@gmail.com>
2026-02-04 20:08:27 +0530

    stream user sync

--- backend/src/lib/inngest.js
+++ backend/src/lib/inngest.js
@@ -1,6 +1,7 @@
 import { Inngest } from "inngest";
 import { connectDB } from "./db.js";
 import User from "../models/User.js";
+import { deleteStreamUser, upsertStreamUser } from "./stream.js";
 
 export const inngest = new Inngest({ id: "meetlio" });
 
@@ -20,6 +21,13 @@ const syncUser = inngest.createFunction(
       profileImage:image_url
     };
     await User.create(newUser)
+    await upsertStreamUser({
+      id:newUser.clerkId.toString(),
+      name:newUser.name,
+      image:newUser.profileImage
+    })
+
+
   },
   
 );
@@ -36,6 +44,7 @@ const deleteUser = inngest.createFunction(
       event.data;
     
     await User.deleteOne({clerkId:id});
+    await deleteStreamUser(id.toString())
 
 
   },
