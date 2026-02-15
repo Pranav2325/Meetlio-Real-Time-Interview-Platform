7fe23b8cb01655ee425e6014265ffc6436583a76
Pranav2325 <pranavdhondkar@gmail.com>
2026-02-15 20:22:56 +0530

    created problem description section

--- frontend/src/lib/utils.js
+++ frontend/src/lib/utils.js
@@ -1,12 +1,15 @@
-export const getDifficultyBadgeClass=(difficulty)=>{
-    switch (difficulty.toLowerCase()) {
-        case "easy":
-           return "badge-success";
-        case "medium":
-           return "badge-warning";
-        case "hard":
-           return "badge-error";
-        default:
-            return "badge-ghost"
-    }
-}
+export const getDifficultyBadgeClass = (difficulty) => {
+  if (!difficulty || typeof difficulty !== "string") {
+    return "badge-ghost";
+  }
+  switch (difficulty.toLowerCase()) {
+    case "easy":
+      return "badge-success";
+    case "medium":
+      return "badge-warning";
+    case "hard":
+      return "badge-error";
+    default:
+      return "badge-ghost";
+  }
+};
